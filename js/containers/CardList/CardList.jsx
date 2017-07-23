7f99ffd59f3f38b7d963e95b34297155288c1fa0
DOMEY Dimitri <dimitri.domey@gmail.com>
2017-07-23 19:29:39 +0200

    Add filter on cards cost

--- js/containers/CardList/CardList.jsx
+++ js/containers/CardList/CardList.jsx
@@ -1,24 +1,68 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import autobind from 'autobind-decorator';
 import Card from '../../components/Card/Card';
 import Loader from '../../components/Loader/Loader';
 import { fetchCards } from "../../actions/cards";
 import './style.scss';
 
 class CardList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: {
+        ammounts: []
+      }
+    }
+  }
   componentWillMount() {
     this.props.dispatch(fetchCards());
   }
+
+  @autobind
+  filterCards(e) {
+    this.setState({
+      filter: {
+        ammounts: [...this.state.filter.ammounts, e.currentTarget.value]
+      }
+    });
+  }
+
+  cards = () => {
+    let cards = this.props.cards.data;
+    const {ammounts} = this.state.filter;
+
+    if (ammounts.length > 0) {
+      cards = cards.filter(card => {
+        return ammounts.indexOf(card.cost ? card.cost.toString() : '0') > -1
+      })
+    }
+
+    return cards;
+  }
+
   render() {
     const { cards } = this.props;
-    return <div>
-      {cards.isFetching ?
-        <Loader /> :
-        <ul className="card-list-container">
-          {cards.data.map(card => <li key={card.cardId}>
-            <Card card={card} />
-          </li>)}
-        </ul>}
+    if (cards.isFetching) {
+      return <Loader/>
+    }
+    return <div className="card-list-container">
+      <ul className="card-list">
+        {this.cards().map(card => <li key={card.cardId}>
+          <Card card={card} />
+        </li>)}
+      </ul>
+      <aside>
+        <section>
+          <header>Filter</header>
+          <div>
+            {(new Array(10)).fill(0).map((k,i) => <label key={i}>
+              <input type="checkbox" name="value" value={i} onChange={this.filterCards}/>
+              {i}
+            </label>)}
+          </div>
+        </section>
+      </aside>
     </div>
   }
 }
